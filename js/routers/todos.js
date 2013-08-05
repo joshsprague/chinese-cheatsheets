c48e1d1ab6e9ec0105849e8687b243e82b4784b8
joshsprague <josh (at) newsplay.org>
2013-08-05 11:15:47 -0700

    API returns results for words with both senses and subsenses

--- js/routers/todos.js
+++ js/routers/todos.js
@@ -1,10 +1,7 @@
 new (Backbone.Router.extend({
   routes: module.routes,
   index: function() {
-    var collection = new Application.Collection([{
-      title: 'First Todo',
-      done: true
-    }]);
+    var collection = new Application.Collection([]);
     var view = new Application.Views["todos/index"]({
       collection: collection
     });
