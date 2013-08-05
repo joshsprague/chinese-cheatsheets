deec8bef080b9848c79017de855889cd64a0300c
joshsprague <jjspragu@asu.edu>
2013-08-05 11:15:47 -0700

    API returns results for words with both senses and subsenses

--- js/views/todos/index.js
+++ js/views/todos/index.js
@@ -4,7 +4,22 @@ Application.View.extend({
     "submit form": function(event) {
       event.preventDefault();
       var attrs = this.serialize();
-      this.collection.add(attrs);
+      var holder;
+      var self = this;
+      $.ajax({
+        url: 'http://api.pearson.com/v2/dictionaries/ldec/entries?headword='+attrs.title+'&apikey=d0Q5fQJA1TLjuY8pGYliGnbKWGmnAy8V',
+        dataType: 'jsonp',
+        success: function (body) {
+          if (body["results"][0]["senses"][0]["translation"] === undefined) {
+            holder = body["results"][0]["senses"][0]["subsenses"][0]["translation"];
+            console.log(holder);
+          } else {
+            holder = body["results"][0]["senses"][0]["translation"];
+            console.log(holder);
+          }
+          self.collection.add({title: attrs.title, translation: holder});
+        }
+      });
       this.$('input[name="title"]').val('');
     },
     'change input[type="checkbox"]': function(event) {
