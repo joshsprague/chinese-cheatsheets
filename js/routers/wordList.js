30c8dcf33d63c7bbb4bed01ea7fc9affa97cb8c7
joshsprague <jjspragu@asu.edu>
2013-08-07 15:34:26 -0700

    Updated wordList model and collections

--- js/routers/wordList.js
+++ js/routers/wordList.js
@@ -1,7 +1,8 @@
 new (Backbone.Router.extend({
   routes: module.routes,
   index: function() {
-    var collection = new Application.Collection([]);
+    var collection = new Application.Collections["wordList/pairs"]([]);
+    debugger;
     var view = new Application.Views["wordList/index"]({
       collection: collection
     });
